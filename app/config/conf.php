c53c6957f8866286c27133233be50e2225dbf1f2
= <=>
2021-05-09 16:15:36 +0800

    updated email configuration and app title

--- app/config/conf.php
+++ app/config/conf.php
@@ -7,15 +7,15 @@ define('APPROOT' , dirname(dirname(__FILE__)));
 define('BASEROOT' , dirname(dirname(dirname(__FILE__))));
 define('VIEWS' , APPROOT.DS.'templates');
 
-define('APP_NAME' , 'JOB RECRUITMENT HRIS (BETA)');
+define('APP_NAME' , 'Recruitment');
 
 const MAILER_AUTH = [
-	'username' => 'demo@monsterthesis.com',
-	'password' => 'UtF3@3r@7Kap2O',
-	'host'     => 'monsterthesis.com',
-	'name'     => 'Thesis Monsters',
-	'replyTo'  => 'sales@monsterthesis.com',
-	'replyToName' => 'Monster Thesis'
+	'username' => 'main@recruitment.host',
+	'password' => 'uc7R8TtBgdEWBRd',
+	'host'     => 'recruitment.host',
+	'name'     => 'Recruitment',
+	'replyTo'  => 'main@recruitment.host',
+	'replyToName' => 'Recruitment'
 ];
 
 define('SCHEDULE_APPOINTMENT' , 'APPOINTMENT SCHEDULE');
