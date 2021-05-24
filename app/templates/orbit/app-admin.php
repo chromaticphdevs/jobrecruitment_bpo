8808f2b55a327d5d8760b9f93361f39e79100953
= <=>
2021-05-24 19:43:14 +0800

    update logo

--- app/templates/orbit/app-admin.php
+++ app/templates/orbit/app-admin.php
@@ -83,8 +83,8 @@
             <div class="sidebar">
                 <!-- Start Logobar -->
                 <div class="logobar">
-                    <a href="catalog.php" class="logo logo-large"><img src="<?php echo _vendor('orbit/uploads/logo_regular.png')?>" class="img-fluid" alt="logo"></a>
-                    <a href="catalog.php" class="logo logo-small"><img src="<?php echo _vendor('orbit/uploads/logo_regular.png')?>" class="img-fluid" alt="logo"></a>
+                    <a href="catalog.php" class="logo logo-large"><img src="<?php echo _public('logo_regular.png')?>" class="img-fluid" alt="logo"></a>
+                    <a href="catalog.php" class="logo logo-small"><img src="<?php echo _public('logo_regular.png')?>" class="img-fluid" alt="logo"></a>
                 </div>
                 <!-- End Logobar -->
                 <!-- Start Navigationbar -->
